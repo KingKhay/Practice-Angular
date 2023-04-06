a6c613c988c61f4b155bd5059b70368af21e4465
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-06 13:14:33 +0000

    added routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {EmployeeComponent} from "./employee/employee.component";
+import {TableComponent} from "./table/table.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employees', component: EmployeeComponent},
+  {path: 'rooms', component: TableComponent},
+  {path: '', redirectTo: 'rooms', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
