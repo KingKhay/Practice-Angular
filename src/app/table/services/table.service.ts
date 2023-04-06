7d75ec77995120b9a94201a3664055237bb69216
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-06 10:42:11 +0000

    catchError and throwError rxjs operators

--- src/app/table/services/table.service.ts
+++ src/app/table/services/table.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpRequest} from "@angular/common/http";
 import {IPerson} from "../../rooms/services/rooms.service";
-import {Observable, shareReplay} from "rxjs";
+import {catchError, Observable, shareReplay, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TableService {
+  handleError = (error: any) => {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent){
+      errorMessage = `Client Side Error :${error.message()}`
+    }
+    else{
+      errorMessage = `Server Side Error :${error.message()}`
+    }
+    return throwError(() => {return errorMessage});
+}
 
   constructor(private http: HttpClient) { }
 
   getPeople(): Observable<IPerson[]>{
-    return this.http.get<IPerson[]>("/api/v1/people").pipe(shareReplay());
+    return this.http.get<IPerson[]>("/api/v1/people").pipe(shareReplay(), catchError(this.handleError));
   }
 
   addPerson(person: IPerson) {
