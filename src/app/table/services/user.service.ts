266f9021f162c66bcc941a2660667cb9d6a1972e
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-07 09:39:11 +0000

    form validation

--- src/app/table/services/user.service.ts
+++ src/app/table/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IPerson} from "../../rooms/services/rooms.service";
+import {shareReplay} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addUser(person: IPerson){
+    console.log("Post Called")
+    return this.http.post<IPerson>('/api/v1/people', person).pipe(shareReplay());
+  }
 }
