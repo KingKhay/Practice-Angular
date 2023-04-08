fd6a9bcecdfbee545f6ac4a577edcb3a91d7830b
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-08 08:20:29 +0000

    added feature modules to reduce app module

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { RoomsRoutingModule } from './rooms-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    RoomsRoutingModule
+    CommonModule
   ]
 })
-export class RoomsModule { }
+export class HeaderModule { }
