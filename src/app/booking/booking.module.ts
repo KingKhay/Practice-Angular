204f258c7c2e6875a51cc38f075296cb344ca5af
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-09 08:50:00 +0000

    refactored reactive form

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import {BookingComponent} from "./booking.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [BookingComponent],
   imports: [
     CommonModule,
-    BookingRoutingModule
+    BookingRoutingModule,
+    ReactiveFormsModule
   ],
   exports: [BookingComponent]
 })
