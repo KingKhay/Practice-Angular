ffd456d7a30286e5bfeea235d97ba33ac289b856
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-09 08:35:34 +0000

    created a reactive form

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-booking',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent {
+  bookingForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.bookingForm = this.fb.group({
+      roomId: [''],
+      guestEmail: [''],
+      guestName: [''],
+      guestCity: [''],
+      guestMobileNumber: [''],
+    })
+  }
 }
