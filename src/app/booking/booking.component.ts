280472e41f028d926a434d198224edcc5179b341
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-10 10:26:02 +0000

    non essential commit

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-booking',
@@ -9,23 +10,31 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class BookingComponent {
   bookingForm!: FormGroup;
 
+
   constructor(private fb: FormBuilder) {
     this.bookingForm = this.fb.group({
-      roomId: [''],
-      guestEmail: [''],
+      roomId: ['',[Validators.required, Validators.minLength(3)], Validators.maxLength(3)],
+      guestEmail: ['', [Validators.required, Validators.email]],
       guestName: [''],
-      guestCity: [''],
+      guestCity: new FormControl(''),
       guestMobileNumber: [''],
       address: this.fb.group({
         addressLine1: [''],
         addressLine2: [''],
         state: [''],
       }),
+      guests: this.fb.array([
+        this.fb.group({
+          name: [''],
+          age: [''],
+        })
+      ])
     })
   }
 
   addBooking() {
     //Use getRawValue instead of value.
     console.log(this.bookingForm.getRawValue())
+
   }
 }
